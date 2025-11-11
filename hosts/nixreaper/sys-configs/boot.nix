7a6e2b43a48aea631e7a30528709674dc73d8ef9
naurias <dev.earmark338@passinbox.com>
2025-11-11 18:40:21 +0500

    remvoved chaotic nix

--- hosts/nixreaper/sys-configs/boot.nix
+++ hosts/nixreaper/sys-configs/boot.nix
@@ -1,30 +1,26 @@
 {
-	config,
-	lib,
-	pkgs,
-	...
-}:
-
-let 
-  zfsCompatibleKernelPackages = lib.filterAttrs (
-    name: kernelPackages:
-    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-    && (builtins.tryEval kernelPackages).success
-    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-   ) pkgs.linuxKernel.packages;
-   latestKernelPackage = lib.last (
-     lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-       builtins.attrValues zfsCompatibleKernelPackages
-       )
-     );
-in
-
-
-{
-
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  zfsCompatibleKernelPackages =
+    lib.filterAttrs (
+      name: kernelPackages:
+        (builtins.match "linux_[0-9]+_[0-9]+" name)
+        != null
+        && (builtins.tryEval kernelPackages).success
+        && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
+    )
+    pkgs.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues zfsCompatibleKernelPackages
+    )
+  );
+in {
   boot.loader.limine.enable = true;
   boot.loader.limine.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = latestKernelPackage;
-
 }
